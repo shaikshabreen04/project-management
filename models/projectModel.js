0a6b7cf099556dd34a0053a49bd7c6afc0eb3645
shaikshabreen04 <shaikshabreen04@gmail.com>
2026-09-06 15:08:21 +0530

    change

--- models/projectModel.js
+++ models/projectModel.js
@@ -11,7 +11,7 @@ const projectSchema = new mongoose.Schema({
         type: String,
         required: true,
         unique: true,
-        trim: true
+        trim: true,
     },
 
     managerId: {
